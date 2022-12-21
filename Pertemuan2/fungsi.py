ac6c3a41991bcfb027c8b34ff5ca96f1d909c9a5
Devi <114374528+Deviyuniarti@users.noreply.github.com>
2022-12-21 04:38:28 -0800

    Add files via upload

---
+++ Pertemuan2/fungsi.py
@@ -0,0 +1,8 @@
+def greeting (jam):
+    if jam < 12 :
+        return " Selamat Pagi!"
+    elif jam < 18 :
+        return " Selamat Sore!"
+    else:
+        return " Selamat Malam"
+print(greeting(6))
---
+++ Pertemuan2/while.py
@@ -0,0 +1,18 @@
+# angka = 1
+
+# while angka > 0 and angka < 11:
+#     angka = int(input("masukan bilangan bulat:"))
+#     if angka >= 1:
+#         print("Bilangan Positif")
+#     elif angka  < 0:
+#         print("Bilangan Negatif")
+#     else:
+#         print("Bilangan Nol")
+
+while True:
+    nama = input("Masukan Nama anda:")
+    if nama =="X":
+        break
+    print("Selamat Datang",nama)
+
+ 
