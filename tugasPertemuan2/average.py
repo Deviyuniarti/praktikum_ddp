9b24b1be39653ac3a979d002fff44c60b98dc04e
Devi <114374528+Deviyuniarti@users.noreply.github.com>
2022-12-21 05:14:49 -0800

    Add files via upload

--- tugasPertemuan2/average.py
+++ tugasPertemuan2/average.py
@@ -12,6 +12,6 @@ hasil = (10+7+(-9.5)+17+(-5)+13.8+4+(-4.2))/8
 print("hasil rata-rata perhitungan nilai adalah: " +str(hasil))
 
 # float:"untuk mencetak bilangan desimal/pecahan baik bilangan negatif maupun positif"
-# Karena pengguna memasukan bilangan sebanyak 4,maka program secara berulang
-# sebanyak 4 kali meminta pengguna  memasukan angka.Angka yang dimasukan adalah 10,7,9.5,17,-5,13.8,-4.2
+# Karena pengguna memasukan bilangan sebanyak 8,maka program secara berulang
+# sebanyak 8 kali meminta pengguna  memasukan angka.Angka yang dimasukan adalah 10,7,9.5,17,-5,13.8,-4.2
 # rata rata dari bilangan tersebut adalah 4.137499999999999,didapat dari (10+7+(-9.5)+17+(-5)+13.8+4+(-4.2))/8.
