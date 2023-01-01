2c13e1a0f381aace8972153bd0a5d8e9c20e8c54
Devi <114374528+Deviyuniarti@users.noreply.github.com>
2023-01-01 16:04:10 +0700

    Delete tugasPertemuan2 directory

--- tugasPertemuan2/mencetak_bilangan.py
+++
@@ -1,6 +0,0 @@
-for i in range(2 , 102, 2):
-    print(i)
-
-# angka 2 yaitu nilai awal
-# angka 102 yaitu nilai akhir,tetapi karena ada perintah membuat selisih 2 berarti akan berhenti diangka 100
-# angka 2 setelah angka 102 merupakan selisih
