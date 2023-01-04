b1b82e065b63bb7e2c595985c586b4d4b7bb9d86
Devi <114374528+Deviyuniarti@users.noreply.github.com>
2023-01-04 03:04:47 -0800

    Add files via upload

---
+++ Pertemuan3/operator.py
@@ -0,0 +1,8 @@
+while True:
+    number = int(input("Masukan Angka "))
+    if number == 0:
+        break
+    if number % 2 == 0:
+        print("Bilangan genap")
+    else :
+        print("Bilangan Ganjil")
